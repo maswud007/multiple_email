bad9bc96541c9caf96dbd6eb8602089f4820cf21
maswud007 <99285655+maswud007@users.noreply.github.com>
2023-08-17 02:38:28 +0600

    Create email.py

---
+++ email.py
@@ -0,0 +1,13 @@
+import smtplib as sb
+
+object = sb.SMTP('smtp.gmail.com',587)
+object.ehlo()
+object.starttls()
+object.login('abdullahalmaswudewu@gmail.com','maswud@123')
+subject = "Testing python to send multiple email"
+body = "I love you tuptup"
+message = "subject:{}\n\n{}".format(subject,body)
+sending_address = ["farzana51-008@diu.edu.bd"]
+object.sendmail('abdullahalmaswudewu@gmail.com',sending_address,message)
+print("email sent successfully.")
+object.quit()
